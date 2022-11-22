4dc9fc6c1c164cf9433e9726057ec4857eb48805
Κωνσταντίνος Καρακασίδης <k.karakasidis@dimos.thermis.local>
2022-11-22 09:48:41 +0200

    Backuper v1.0.0

---
+++ dist/Backuper/RUN.ps1
@@ -0,0 +1,2 @@
+﻿.\main.exe >> "\\10.64.132.250\Shares\it\Backup logs\$(get-date -f dd-MM-yyyy)_log.txt"
+pause
---
+++ main.py
@@ -0,0 +1,23 @@
+from dirsync import sync
+import configparser
+import os
+
+
+def main():
+    paths = 'options.cfg'
+    if os.path.isfile(paths):
+
+        config = configparser.ConfigParser()
+        config.read(paths)
+        source_path = config.get('DEFAULT','source')
+        target_path = config.get('DEFAULT','target')
+        option = 'sync'
+
+        sync(r"{}".format(source_path), r"{}".format(target_path), option, verbose=True, purge=True) #for syncing one way + purge
+
+    else:
+        print(f'Δεν βρέθηκε το {paths}')
+
+
+if __name__=="__main__":
+    main()
