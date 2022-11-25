9f4224e80c29e2735fb42194e47751401e2f1fc7
Κωνσταντίνος Καρακασίδης <k.karakasidis@dimos.thermis.local>
2022-11-25 08:55:35 +0200

    Backuper v1.0.1

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#Backuper έκδοση 1.0.1 Κωνσταντίνος Καρακασίδης
+
 from dirsync import sync
 import configparser
 import os
@@ -13,7 +15,7 @@ def main():
         target_path = config.get('DEFAULT','target')
         option = 'sync'
 
-        sync(r"{}".format(source_path), r"{}".format(target_path), option, verbose=True, purge=True) #for syncing one way + purge
+        sync(r'{}'.format(source_path), r'{}'.format(target_path), option, verbose=True, purge=True) #for syncing one way + purge
 
     else:
         print(f'Δεν βρέθηκε το {paths}')
